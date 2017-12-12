bcf112f335c8f42e2f8b7634505fffa0df4fbc00
Mohit Rathi <mohit.rathi@timesinternet.in>
2017-12-12 16:07:56 +0530

    integrating redux in react example

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
-
-export default class App extends Component {
-    render () {
-        return <p>This is my new react app</p>
-    }
+import InputPreview from '../components/InputPreview';
+import {connect} from 'react-redux';
+import {setMessage} from '../actions/message';
+class App extends Component {
+  _onChange = (value) => {
+    this.props.dispatch(setMessage(value))
+  }
+  render () {
+    const {message} = this.props.messageReducer;
+    return (
+       <InputPreview
+         value={message}
+         onChange={this._onChange}/>
+    )
+  }
 }
+export default connect(state => state)(App);
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 // main app
 import App from './containers/App';
-
-ReactDOM.render(<App />, document.getElementById('app'))
+import { Provider } from 'react-redux';
+import store from './store';
+ReactDOM.render(
+ <Provider store={store}>
+ <App />
+ </Provider>,
+ document.getElementById('app')
+)
